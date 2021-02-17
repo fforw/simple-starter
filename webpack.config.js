4628fd98b14bd7ccf3b9321be9d70894b0693faf
Sven Helmberger <fforw@gmx.de>
2021-02-17 17:15:16 +0100

    Upgrade libs, modernize webpack
    Upgrade libs to newest version.
    
    Modernize the command line invocation of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ const fs = require("fs");
 const webpack = require("webpack");
 const shellJs = require("shelljs");
 
-const PRODUCTION = (process.env.NODE_ENV === "production");
 const STATS = (!!process.env.STATS);
 
 const JS_OUTPUT_DIRECTORY = path.join(__dirname, "docs/");
@@ -18,121 +17,126 @@ if (!fs.existsSync(JS_OUTPUT_DIRECTORY))
     shellJs.mkdir("-p", JS_OUTPUT_DIRECTORY);
 }
 
-module.exports = {
-    mode: process.env.NODE_ENV,
-    entry: {
-        "main": "./src/index.js",
-        "test": "./src/test.js"
-    },
+module.exports = (env,argv) => {
+    const PRODUCTION = (argv.mode === "production");
+    return (
+        {
 
-    devtool: "source-map",
+            mode: process.env.NODE_ENV,
+            entry: {
+                "main": "./src/index.js",
+                "test": "./src/test.js"
+            },
 
-    output: {
-        path: JS_OUTPUT_DIRECTORY,
-        filename: "bundle-[name]-[chunkhash].js",
-        chunkFilename: "bundle-[name]-[chunkhash].js",
+            devtool: "source-map",
 
-        library: "Demo",
-        libraryTarget: "var"
-    },
+            output: {
+                path: JS_OUTPUT_DIRECTORY,
+                filename: "bundle-[name]-[chunkhash].js",
+                chunkFilename: "bundle-[name]-[chunkhash].js",
+
+                library: "Demo",
+                libraryTarget: "var"
+            },
 
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            inject: "body",
-            chunks: ["vendors", "main"],
-            template: "src/template.html",
-            filename: "index.html"
-        }),
+            plugins: [
+                new HtmlWebpackPlugin({
+                    inject: "body",
+                    chunks: ["vendors", "main"],
+                    template: "src/template.html",
+                    filename: "index.html"
+                }),
 
-        new HtmlWebpackPlugin({
-            inject: "body",
-            chunks: ["vendors", "test"],
-            template: "src/template.html",
-            filename: "test.html"
-        }),
+                new HtmlWebpackPlugin({
+                    inject: "body",
+                    chunks: ["vendors", "test"],
+                    template: "src/template.html",
+                    filename: "test.html"
+                }),
 
-        new MiniCssExtractPlugin({
-            filename: "bundle-[name]-[chunkhash].css",
-            chunkFilename: "bundle-[id]-[chunkhash].css"
-        }),
+                new MiniCssExtractPlugin({
+                    filename: "bundle-[name]-[chunkhash].css",
+                    chunkFilename: "bundle-[id]-[chunkhash].css"
+                }),
 
-        new webpack.DefinePlugin({
-            "__PROD": PRODUCTION,
-            "__DEV": !PRODUCTION,
-            "__STATS": STATS,
-            "process.env.NODE_ENV": JSON.stringify(PRODUCTION ? "production" : "development")
-        }),
+                new webpack.DefinePlugin({
+                    "__PROD": PRODUCTION,
+                    "__DEV": !PRODUCTION,
+                    "__STATS": STATS
+                }),
 
-        new CopyPlugin({
-                patterns: [
-                    {
-                        from: "media/**/*"
+                new CopyPlugin({
+                        patterns: [
+                            {
+                                from: "media/**/*"
+                            }
+                        ]
                     }
-                ]
-            }
-        ),
+                ),
 
-        new CleanObsoleteChunks()
-    ],
+                new CleanObsoleteChunks()
+            ],
 
-    module: {
-        rules: [
-            // babel transpilation ( see .babelrc for babel config)
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-
-            // this is just concatenating the .css modules in components to one bundle.
-            // No postprocessing of that.
-            {
-                test: /\.css$/,
-                //                exclude: /node_modules/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"]
-            },
-
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: [
+            module: {
+                rules: [
+                    // babel transpilation ( see .babelrc for babel config)
                     {
-                        loader: "file-loader",
-                        options: {}
+                        test: /\.js$/,
+                        exclude: /node_modules/,
+                        use: {
+                            loader: "babel-loader"
+                        }
+                    },
+
+                    // this is just concatenating the .css modules in components to one bundle.
+                    // No postprocessing of that.
+                    {
+                        test: /\.css$/,
+                        //                exclude: /node_modules/,
+                        use: [MiniCssExtractPlugin.loader, "css-loader"]
+                    },
+
+                    {
+                        test: /\.(png|jpg|gif)$/,
+                        use: [
+                            {
+                                loader: "file-loader",
+                                options: {}
+                            }
+                        ]
+                    },
+
+                    {
+                        test: /\.(glsl|frag|vert)$/,
+                        exclude: /node_modules/,
+                        use: [
+                            "raw-loader",
+                            {
+                                loader: "glslify-loader",
+                                options: {
+                                    transform: [
+                                        "glslify-hex"
+                                    ]
+                                }
+                            }
+                        ]
                     }
+
                 ]
             },
 
-            {
-                test: /\.(glsl|frag|vert)$/,
-                exclude: /node_modules/,
-                use: [
-                    "raw-loader",
-                    {
-                        loader: "glslify-loader",
-                        options: {
-                            transform: [
-                                "glslify-hex"
-                            ]
+            optimization: {
+                splitChunks: {
+                    cacheGroups: {
+                        commons: {
+                            test: /[\\/]node_modules[\\/]/,
+                            name: "vendors",
+                            chunks: "all"
                         }
                     }
-                ]
-            }
-
-        ]
-    },
-
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                commons: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: "vendors",
-                    chunks: "all"
                 }
             }
         }
-    }
+    )
 };
